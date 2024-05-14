518b66376116bc05a67049b5a22a5000cab73031
genildo <genildoifpb@gmail.com>
2024-05-14 17:22:55 -0300

    main

--- Progressao/paginas/urls.py
+++ Progressao/paginas/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib import admin
 from .views import IndexView
+from .views import  PaginaInicial
 urlpatterns = [
+    path('modelo/', PaginaInicial.as_view(), name='modelo'),
     path('admin/', admin.site.urls),
     path('', IndexView.as_view(), name='inicio'),
 ]
--- Progressao/paginas/views.py
+++ Progressao/paginas/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'index.html'
+class PaginaInicial(TemplateView):
+    template_name = 'modelo.html'
+
+
