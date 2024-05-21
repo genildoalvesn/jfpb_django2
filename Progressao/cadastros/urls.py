80e39cfc66feb4b1c37285de284eee2d876fbc86
genildo <genildoifpb@gmail.com>
2024-05-21 17:37:32 -0300

    aproveitamento de paginas e incremento de formularios

--- Progressao/cadastros/urls.py
+++ Progressao/cadastros/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib import admin
-
-from Progressao.paginas.views import PaginaInicial, SobreView, IndexView
+from .views import CampoCreate, AtividadeCreate
+from paginas.views import PaginaInicial, SobreView, IndexView
 
 urlpatterns = [
+    path('cadastrar/campo/', CampoCreate.as_view(), name="cadastrar-campo"),
+    path('cadastrar/atividade/', AtividadeCreate.as_view(), name="cadastrar-atividade"),
     path('modelo/', PaginaInicial.as_view(), name='modelo'),
     path('sobre/', SobreView.as_view(), name='sobre'),
     path('admin/', admin.site.urls),
--- Progressao/cadastros/views.py
+++ Progressao/cadastros/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from .models import Campo, Atividade
+from django.urls import reverse_lazy
 # Create your views here.
+class CampoCreate(CreateView):
+    model = Campo
+    fields = ['name', 'descricao']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+class AtividadeCreate(CreateView):
+    model = Atividade
+    fields = ['numero','descricao', 'pontos', 'detalhes', 'campo']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
