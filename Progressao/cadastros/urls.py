c4fa26ea7b63e2538b984c03cf5a8444ae5226d9
genildo <genildoifpb@gmail.com>
2024-05-22 14:44:01 -0300

    cadastro_campo_e_intercao_com_template

--- Progressao/cadastros/urls.py
+++ Progressao/cadastros/urls.py
@@ -4,7 +4,7 @@ from .views import CampoCreate, AtividadeCreate
 from paginas.views import PaginaInicial, SobreView, IndexView
 
 urlpatterns = [
-    path('cadastrar/campo/', CampoCreate.as_view(), name="cadastrar-campo"),
+    path('cadastrar/campo', CampoCreate.as_view(), name="cadastrar-campo"),
     path('cadastrar/atividade/', AtividadeCreate.as_view(), name="cadastrar-atividade"),
     path('modelo/', PaginaInicial.as_view(), name='modelo'),
     path('sobre/', SobreView.as_view(), name='sobre'),
